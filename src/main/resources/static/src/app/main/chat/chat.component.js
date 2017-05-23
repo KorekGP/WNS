11ebb4859d7026cdb8f403e1e1962e8cf7c91e3d
KorekGP <grzegorz.podgorski@apilia.pl>
2017-05-23 15:19:25 +0200

    TSIL-57

--- src/main/resources/static/src/app/main/chat/chat.component.js
+++ src/main/resources/static/src/app/main/chat/chat.component.js
@@ -4,6 +4,28 @@
 import chatHtml from './chat.component.html';
 import './chat.component.scss';
 
+class chatController {
+
+    constructor($http) {
+        let messages = {};
+        $http.get('/chat').then(function (data) {
+            messages = data;
+        })
+    }
+
+    sendMessage() {
+        $http.post('/chat', {
+            content: this.content
+        }).then(
+            function (data) {
+                console.log(data);
+            }
+        )
+    }
+}
+
 export default {
-    template: chatHtml
+    template: chatHtml,
+    controller: chatController,
+    controllerAs: 'chatCtrl'
 };
---
+++ src/main/resources/static/src/app/main/chat/chat.repo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Grzegorz on 23.05.2017.
+ */
+
+export class chatRepository {
+
+    getMessages() {
+        return
+    }
+}
