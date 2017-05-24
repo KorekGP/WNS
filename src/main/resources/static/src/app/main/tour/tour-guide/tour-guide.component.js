bd619ea0c8be84a282cb15a6d650b1ae8ae900e1
Eryk Mariankowski <dagi12@o2.pl>
2017-05-24 17:34:23 +0200

    TSIL-69 TSIL-68

--- src/main/resources/static/src/app/main/tour/tour-guide/tour-guide.component.js
+++ src/main/resources/static/src/app/main/tour/tour-guide/tour-guide.component.js
@@ -3,9 +3,19 @@
  */
 import tourGuideHtml from './tour-guide.component.html';
 import './tour-guide.component.css';
-import uiRouter from '@uirouter/angularjs';
 import imgPath1 from '../../../images/01a_1.jpg';
-
+import imgPath2 from '../../../images/02aa_1.jpg';
+import imgPath3 from '../../../images/02bb_1.jpg';
+import imgPath4 from '../../../images/03a_1.jpg';
+import imgPath5 from '../../../images/06a_1.jpg';
+import imgPath6 from '../../../images/06b_1.jpg';
+import imgPath7 from '../../../images/08a_1.jpg';
+import imgPath8 from '../../../images/09a_1.jpg';
+import imgPath9 from '../../../images/09b_1.jpg';
+import imgPath10 from '../../../images/09b_1_(1).jpg';
+import imgPath11 from '../../../images/10a_1.jpg';
+import imgPath12 from '../../../images/sala_wykładowa bud_E.jpg';
+import imgPath13 from '../../../images/sala_wykładowa_ budynek_E.jpg';
 
 class TourGuideController {
 
@@ -13,30 +23,30 @@ class TourGuideController {
 
         this.buildingID = $stateParams.buildingID;
 
-        if (this.buildingID == 1) {
+        if (this.buildingID === 1) {
             this.slides = [
                 {image: imgPath1, description: 'Image 00'},
-                {image: 'src/app/images/02aa_1.jpg', description: 'Image 01'},
-                {image: 'src/app/images/02bb_1.jpg', description: 'Image 02'},
-                {image: 'src/app/images/03a_1.jpg', description: 'Image 03'},
+                {image: imgPath2, description: 'Image 01'},
+                {image: imgPath3, description: 'Image 02'},
+                {image: imgPath4, description: 'Image 03'},
             ];
-        } else if (this.buildingID == 2 ) {
+        } else if (this.buildingID === 2) {
             this.slides = [
-                {image: 'src/app/images/06a_1.jpg', description: 'Image 00'},
-                {image: 'src/app/images/06b_1.jpg', description: 'Image 01'},
-                {image: 'src/app/images/08a_1.jpg', description: 'Image 02'},
+                {image: imgPath5, description: 'Image 00'},
+                {image: imgPath6, description: 'Image 01'},
+                {image: imgPath7, description: 'Image 02'},
             ];
-        } else if (this.buildingID == 3 ) {
+        } else if (this.buildingID === 3) {
             this.slides = [
-                {image: 'src/app/images/09a_1.jpg', description: 'Image 00'},
-                {image: 'src/app/images/09b_1.jpg', description: 'Image 01'},
-                {image: 'src/app/images/09b_1_(1).jpg', description: 'Image 02'},
+                {image: imgPath8, description: 'Image 00'},
+                {image: imgPath9, description: 'Image 01'},
+                {image: imgPath10, description: 'Image 02'},
             ];
-        } else if (this.buildingID == 4 ) {
+        } else if (this.buildingID === 4) {
             this.slides = [
-                {image: 'src/app/images/10a_1.jpg', description: 'Image 00'},
-                {image: 'src/app/images/sala_wykładowa bud_E.jpg', description: 'Image 01'},
-                {image: 'src/app/images/sala_wykładowa_ budynek_E.jpg', description: 'Image 02'},
+                {image: imgPath11, description: 'Image 00'},
+                {image: imgPath12, description: 'Image 01'},
+                {image: imgPath13, description: 'Image 02'},
             ];
         }
 
--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -4,7 +4,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-import agGrid from 'ag-grid';
+import agGrid from 'ag-grid/dist/ag-grid.min.noStyle';
 agGrid.initialiseAgGridWithAngular1(angular);
 
 import './app/app.module';
